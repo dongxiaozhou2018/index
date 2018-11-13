7c02e3b50edbaf32e7657b88751836e5baf85beb
mr-d9 <836796843@qq.com>
2018-11-13 20:52:34 +0800

    注册信息提交

---
+++ js/homework.js
@@ -0,0 +1,98 @@
+window.onload = function ()
+{
+   //用户名验证
+    var username = document.getElementById("username");
+    username.onblur = function ()
+    {
+        var result = this.value.match(/^[a-z0-9]{6,14}$/);
+        if (result == null)
+        {
+            document.getElementById("span1").innerText ="请输入用户名";
+        }else
+        {
+            document.getElementById("span1").innerText ="√";
+        }
+    }
+    //
+    var password = document.getElementById("password");
+    password.onblur = function ()
+    {
+        var result = this.value.match(/^\w{6,14}$/);
+        if (result == null)
+        {
+            document.getElementById("span2").innerText ="请输入6~14位密码";
+        }else
+        {
+            document.getElementById("span2").innerText ="√";
+        }
+        if (result == null )
+        {
+            document.getElementById("span3").innerText ="密码不匹配";
+        }
+    }
+    var repassword = document.getElementById("repassword");
+    repassword.onblur = function ()
+    {
+        if (repassword.value != password.value )
+        {
+            document.getElementById("span3").innerText ="密码不匹配";
+        }else
+        {
+            document.getElementById("span3").innerText ="√";
+        }
+
+    }
+    //邮箱验证
+    var email = document.getElementById("email");
+    email.onblur = function ()
+    {
+        var result = this.value.match(/^[0-9]{6,14}@(qq|163|126)(\.com|\.cn|\.org)$/);
+        if (result == null)
+        {
+            document.getElementById("span4").innerText ="邮箱不可用";
+        }else
+        {
+            document.getElementById("span4").innerText ="√";
+        }
+    }
+
+    //手机号码
+    var tel = document.getElementById("tel");
+    tel.onblur = function ()
+    {
+        var result = this.value.match(/^[0-9]{11}$/);
+        if (result == null)
+        {
+            document.getElementById("span5").innerText ="手机号有误";
+        }else
+        {
+            document.getElementById("span5").innerText ="√";
+        }
+    }
+    //邮编
+    var zipcode = document.getElementById("zipcode");
+    zipcode.onblur = function ()
+    {
+        var result = this.value.match(/^[0-9]{6}$/);
+        if (result == null)
+        {
+            document.getElementById("span6").innerText ="邮编有误";
+        }else
+        {
+            document.getElementById("span6").innerText ="√";
+        }
+    }
+    //身份证号
+    var id = document.getElementById("id");
+    id.onblur = function ()
+    {
+        var result = this.value.match(/^[0-9]{17}([0-9]|x)$/);
+        if (result == null)
+        {
+            document.getElementById("span7").innerText ="身份证有误";
+        }else
+        {
+            document.getElementById("span7").innerText ="√";
+        }
+    }
+}
